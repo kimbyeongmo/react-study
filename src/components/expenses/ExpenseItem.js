b376d9143b8ba4a17a1fab8abbf3a4fe8b23e981
kimbyeongmo <kbmomo9999@naver.com>
2024-06-12 12:24:36 +0900

    # 이벤트 바인딩

--- src/components/expenses/ExpenseItem.js
+++ src/components/expenses/ExpenseItem.js
@@ -1,9 +1,10 @@
-import React from "react";
-import "./ExpenseItem.css";
-import ExpenseDate from "./ExpenseDate";
-import Card from "../UI/Card";
+import React from 'react'
+import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate';
+import Card from '../UI/Card';
 
 const ExpenseItem = ({ date, title, price: exPrice }) => {
+
   // console.log('props: ', aaa);
 
   // 변수 선언
@@ -15,10 +16,11 @@ const ExpenseItem = ({ date, title, price: exPrice }) => {
 
   // 1자리 숫자를 2자리수로 변환하는 함수
   const make2digit = (text) => {
-    return text.toString().padStart(2, "0");
+    return text.toString().padStart(2, '0');
   };
 
   const makeFormattedDate = () => {
+
     const year = date.getFullYear();
     const month = date.getMonth();
     const day = date.getDate();
@@ -27,17 +29,30 @@ const ExpenseItem = ({ date, title, price: exPrice }) => {
   };
 
   // 원화 표기법으로 변환
-  const formattedPrice = new Intl.NumberFormat("ko-KR").format(exPrice);
+  const formattedPrice = new Intl.NumberFormat('ko-KR').format(exPrice);
+
+  // 이벤트 핸들러 선언
+  const clickHandler = e => {
+    console.log('클릭함!');
+    console.log(e.target.previousElementSibling.firstElementChild.textContent);
+
+    // const $price = document.querySelectorAll('.expense-item__price');
+    // console.log($price);
+    console.log(this);
+  };
+
 
   return (
-    <Card className="expense-item">
-      <ExpenseDate exDate={date} />
-      <div className="expense-item__description">
-        <h2>{title}</h2>
-        <div className="expense-item__price">{formattedPrice}원</div>
-      </div>
-    </Card>
-  );
-};
+      <Card className='expense-item'>
+        <ExpenseDate exDate={date} />
+        <div className='expense-item__description'>
+          <h2>{title}</h2>
+          <div className='expense-item__price'>{formattedPrice}원</div>
+        </div>
+        <button id='btn' onClick={clickHandler}>버튼1</button>
+        <button id='btn2' onMouseOver={e =>alert('하하')}>버튼2</button>
+      </Card>
+  )
+}
 
-export default ExpenseItem;
+export default ExpenseItem
