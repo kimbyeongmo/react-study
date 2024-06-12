50b27edac1735a7df699d812e4d3ec8b79b9fca8
kimbyeongmo <kbmomo9999@naver.com>
2024-06-12 11:02:10 +0900

    # react props1

--- src/App.js
+++ src/App.js
@@ -20,9 +20,9 @@ const App=() => {
 
   return (
     <>
-      <Expenseltem />
-      <Expenseltem />
-      <Expenseltem />
+      <Expenseltem title="치킨먹음" price={30000} date={new Date(2024,6,12)}/>
+      <Expenseltem title="족발먹음" price={40000} date={new Date(2024,6,12)}/>
+      <Expenseltem title="헬스장등록" price={300000} date={new Date(2024,6,12)}/>
     </>
   );
 }
