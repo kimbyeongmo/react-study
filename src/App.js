395e0529ce9de3881d5a6a06d3b475faf2dd34fd
kimbyeongmo <kbmomo9999@naver.com>
2024-06-12 12:00:50 +0900

    # react props2

--- src/App.js
+++ src/App.js
@@ -1,30 +1,34 @@
-
-import './App.css';
 import React from 'react';
-import Expenseltem from './components/expenses/Expenseltem';
+import './App.css';
+import ExpenseList from './components/expenses/ExpenseList';
+import Greet from './components/Greet';
 
-const App=() => {
+const App = () => {
 
-  // jsx 문법
-  // const $h2 = React.createElement('h2', null, '방가방가햄토리');
-  const $h2 = <h2>방가봉가햄톨이</h2>;
-
-  // jsx 규칙 
-  // 1. return안에있는 태그는 반드시 하나의 태그로 묶여야 함.
-  // 2. 빈 태그(닫는 태그가 없는)는 반드시 /> 로 마감
-  // 3. 태그의 class속성은 자바스크립트 키워드 class와 겹쳐서 className으로 표기
-  // 4. 의미없는 부모는 <React.Fragment>로 감싸면 됨
-  // 5. 변수값이나 함수를 출력할 때는 {}로 감싸면 됨.
-
-  const hello = '안녕안녕?';
+  // 서버에서 지출항목 JSON 배열을 응답받음
+  const expenses = [
+    {
+      title: '치킨먹음',
+      price: 30000,
+      date: new Date(2024, 6 -1, 3)
+    },
+    {
+      title: '족발먹음',
+      price: 40000,
+      date: new Date(2024, 6 -1, 7)
+    },
+    {
+      title: '헬스장등록',
+      price: 300000,
+      date: new Date(2024, 6 -1, 12)
+    },
+  ];
 
   return (
     <>
-      <Expenseltem title="치킨먹음" price={30000} date={new Date(2024,6,12)}/>
-      <Expenseltem title="족발먹음" price={40000} date={new Date(2024,6,12)}/>
-      <Expenseltem title="헬스장등록" price={300000} date={new Date(2024,6,12)}/>
+      <ExpenseList expenses={expenses} />
     </>
   );
-}
+};
 
 export default App;
--- src/components/expenses/ExpenseItem.js
+++ src/components/expenses/ExpenseItem.js
@@ -1,8 +1,9 @@
-import React from 'react'
-import './ExpenseItem.css';
+import React from "react";
+import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
+import Card from "../UI/Card";
 
 const ExpenseItem = ({ date, title, price: exPrice }) => {
-
   // console.log('props: ', aaa);
 
   // 변수 선언
@@ -14,11 +15,10 @@ const ExpenseItem = ({ date, title, price: exPrice }) => {
 
   // 1자리 숫자를 2자리수로 변환하는 함수
   const make2digit = (text) => {
-    return text.toString().padStart(2, '0');
+    return text.toString().padStart(2, "0");
   };
 
   const makeFormattedDate = () => {
-
     const year = date.getFullYear();
     const month = date.getMonth();
     const day = date.getDate();
@@ -27,17 +27,17 @@ const ExpenseItem = ({ date, title, price: exPrice }) => {
   };
 
   // 원화 표기법으로 변환
-  const formattedPrice = new Intl.NumberFormat('ko-KR').format(exPrice);
+  const formattedPrice = new Intl.NumberFormat("ko-KR").format(exPrice);
 
   return (
-    <div className='expense-item'>
-      <div>{makeFormattedDate()}</div>
-      <div className='expense-item__description'>
+    <Card className="expense-item">
+      <ExpenseDate exDate={date} />
+      <div className="expense-item__description">
         <h2>{title}</h2>
-        <div className='expense-item__price'>{formattedPrice}원</div>
+        <div className="expense-item__price">{formattedPrice}원</div>
       </div>
-    </div>
-  )
-}
+    </Card>
+  );
+};
 
-export default ExpenseItem
+export default ExpenseItem;
