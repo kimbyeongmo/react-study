6b2fb43d72d6c2601c87695c5078dfb200554a20
kimbyeongmo <kbmomo9999@naver.com>
2024-06-13 12:44:52 +0900

    # 동적 리스트 렌더링

--- src/App.js
+++ src/App.js
@@ -15,12 +15,22 @@ const App = () => {
     {
       title: '족발먹음',
       price: 40000,
-      date: new Date(2024, 6 -1, 7)
+      date: new Date(2023, 12 -1, 7)
     },
     {
       title: '헬스장등록',
       price: 300000,
       date: new Date(2024, 6 -1, 12)
+    },
+    {
+      title: '파파존스피자',
+      price: 40000,
+      date: new Date(2022, 3 -1, 4)
+    },
+    {
+      title: '파리채',
+      price: 1500,
+      date: new Date(2023, 5 -1, 6)
     },
   ];
 
--- src/components/expenses/ExpenseFilter.js
+++ src/components/expenses/ExpenseFilter.js
@@ -1,21 +1,21 @@
 import React from 'react';
-import './EXpenseFilter.css';
+import './ExpenseFilter.css';
+
+const ExpenseFilter = ({ onChangeFilter }) => {
 
 
+  const changeYearHandler = e => {
+    const selectedYear = e.target.value;
+    // console.log(selectedYear);
 
-const ExpenseFilter = ({onChangeFilter}) => {
-
-  const changeyearHandler = e=>{
-    const selectedYear = e.target.value
     onChangeFilter(selectedYear);
-  }
-
+  };
 
   return (
     <div className="expenses-filter">
       <div className="expenses-filter__control">
         <label>Filter by year</label>
-        <select onChange={changeyearHandler}>
+        <select onChange={changeYearHandler}>
           <option value="2024">2024</option>
           <option value="2023">2023</option>
           <option value="2022">2022</option>
