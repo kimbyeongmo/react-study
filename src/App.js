195153df362072f30a3bb01f72896d562b44dd9b
kimbyeongmo <kbmomo9999@naver.com>
2024-06-14 09:25:13 +0900

    # 지출항목 배열 상태값으로 관리

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import ExpenseList from './components/expenses/ExpenseList';
 import NewExpense from './components/new-expense/NewExpense';
@@ -10,48 +10,45 @@ const App = () => {
     {
       title: '치킨먹음',
       price: 30000,
-      date: new Date(2024, 6 -1, 3)
+      date: new Date(2024, 6 - 1, 3)
     },
     {
       title: '족발먹음',
       price: 40000,
-      date: new Date(2023, 12 -1, 7)
+      date: new Date(2023, 12 - 1, 17)
     },
     {
       title: '헬스장등록',
       price: 300000,
-      date: new Date(2024, 6 -1, 12)
+      date: new Date(2024, 6 - 1, 12)
     },
     {
       title: '파파존스피자',
       price: 40000,
-      date: new Date(2022, 3 -1, 4)
+      date: new Date(2022, 3 - 1, 4)
     },
     {
       title: '파리채',
       price: 1500,
-      date: new Date(2023, 5 -1, 6)
+      date: new Date(2023, 5 - 1, 6)
     },
   ];
 
+  // 배열을 상태변수로 관리
+  const [expenseList, setExpenseList] = useState(expenses); 
+
+
   // ExpenseForm에게 내려보낼 함수
-  const onAddExpense = (userInput)=>{
-    console.log(`App.js가 내려보낸 함수 호출!`);
-    console.log(userInput);
-
-    expenses.push(userInput);
-
-    console.log(expenses);
+  const onAddExpense = (userInput) => {
+    setExpenseList([...expenseList, userInput]);
   };
 
   return (
-    <>      
+    <>
       <NewExpense onSave={onAddExpense} />
-      <ExpenseList expenses={expenses} />
+      <ExpenseList expenses={expenseList} />
     </>
   );
 };
 
 export default App;
-
- 
