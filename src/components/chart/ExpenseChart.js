27d99240a02a92454e5c1dd3c8b7ef7610ee263e
kimbyeongmo <kbmomo9999@naver.com>
2024-06-14 11:35:22 +0900

    ~ # 목표 설정 앱 템플릿

--- src/components/chart/ExpenseChart.js
+++ src/components/chart/ExpenseChart.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import Chart from './Chart';
 
-const ExpenseChart = () => {
-  
+const ExpenseChart = ({ expenses }) => {
+
   const chartDataPoints = [
     { label: 'Jan', value: 0 },
     { label: 'Feb', value: 0 },
@@ -18,7 +18,23 @@ const ExpenseChart = () => {
     { label: 'Dec', value: 0 },
   ];
 
-  return <Chart dataPoints={chartDataPoints}/>;
+  /*
+    - 해당 연도의 모든 지출데이터배열에서 월별로 지출액을 합산하여
+      chartDataPoints배열에 value값에 누적 저장
+  */
+
+  expenses.forEach(exp => {
+    // 지출액을 추출해서 chartDataPoints배열에 해당 월에 value에 합산
+    // 지출액
+    const expensePrice = exp.price;
+    const expenseMonth = exp.date.getMonth();
+
+    chartDataPoints[expenseMonth].value += expensePrice;
+  });
+
+  console.log('cdp: ', chartDataPoints);
+
+  return <Chart dataPoints={chartDataPoints} />;
 };
 
 export default ExpenseChart;
