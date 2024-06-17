b045536abc793c1ab39364cf9df4c60256addb17
kimbyeongmo <kbmomo9999@naver.com>
2024-06-17 10:48:24 +0900

    # 유저관리 기본 템플릿 생성

--- src/App.js
+++ src/App.js
@@ -1,64 +1,15 @@
-import React, { useState } from 'react';
-import CourseInput from './components/CourseGoals/CourseInput';
-import CourseList from './components/CourseGoals/CourseList';
+import React, { Fragment, useState } from 'react';
 import './App.css';
+import AddUsers from './components/Users/AddUsers';
 
-// 기본 더미 데이터
-const DUMMY_DATA = [
-  {
-    id: 'g1',
-    text: '리액트 컴포넌트 스타일링 마스터하기'
-  },
-  {
-    id: 'g2',
-    text: 'UI/UX 프로그래밍 쌉고수되기'
-  },
-];
 
 const App = () => {
 
-  const [goals, setGoals] = useState(DUMMY_DATA);
-
-  // CourseInput에게 전달할 함수
-  const addGoalHandler = (goalObject) => {
-    setGoals([...goals, goalObject]);
-  };
-
-  // CouseItem에게 전달할 함수
-  const deleteGoalHandler = (id) => {
-    // console.log('id: ', id);
-
-    // let index = -1;
-    // for (let i = 0; i < goals.length; i++) {
-    //   if (goals[i].id === id) {
-    //     index = i;
-    //     break;
-    //   }
-    // }
-    // // console.log('index: ', index);
-
-    // goals.splice(index, 1);
-
-    // setGoals([...goals]);
-
-
-
-    // goals.splice(goals.findIndex(g=>g.id===id,1)
-
-
-
-    setGoals(goals.filter(g=>g.id!==id));
-  };
-
   return (
-    <div>
-      <section id="goal-form">
-        <CourseInput onAdd={addGoalHandler} />
-      </section>
-      <section id="goals">
-        <CourseList items={goals} onDelete={deleteGoalHandler} />
-      </section>
-    </div>
+    <>
+     <AddUsers /> 
+     <UserList />
+    </>
   );
 };
 
