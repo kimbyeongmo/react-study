b2c84b1bc0dace8f63745e1392a4fcf51ad28659
kimbyeongmo <kbmomo9999@naver.com>
2024-06-18 09:49:07 +0900

    # 로그인시 발생하는 side effect

--- src/components/SideEffect/Login.js
+++ src/components/SideEffect/Login.js
@@ -45,8 +45,10 @@ const Login = ({ onLogin }) => {
     setPasswordIsValid(enteredPassword.trim().length > 6);
   };
 
+  // 로그인 버튼을 눌렀을 때 이벤트 핸들러
   const submitHandler = (e) => {
     e.preventDefault();
+    // app.js에서 받은 로그인핸들러 호출
     onLogin(enteredEmail, enteredPassword);
   };
 
