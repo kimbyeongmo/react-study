a2c24c653cb8d437a799abf1cf60e9e2f1173131
kimbyeongmo <kbmomo9999@naver.com>
2024-06-18 09:30:07 +0900

    # 로그인 기능 동작 주석

--- src/components/SideEffect/Login.js
+++ src/components/SideEffect/Login.js
@@ -5,10 +5,20 @@ import styles from './Login.module.css';
 import Button from '../UI/Button';
 
 const Login = ({ onLogin }) => {
+
+  // 사용자가 입력한 이메일을 상태관리
   const [enteredEmail, setEnteredEmail] = useState('');
+
+  // 이메일 입력값이 정상인지 유무 확인
   const [emailIsValid, setEmailIsValid] = useState();
+
+  // 사용자가 입력한 패스워드를 상태관리
   const [enteredPassword, setEnteredPassword] = useState('');
+
+  // 패스워드 입력값이 정상인지 유무 확인
   const [passwordIsValid, setPasswordIsValid] = useState();
+
+  // 이메일, 패스워드가 둘 다 정상인지 확인
   const [formIsValid, setFormIsValid] = useState(false);
 
   const emailChangeHandler = (e) => {
