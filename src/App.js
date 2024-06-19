4556be02b3809e552d4d8d18febd3c31409e04ea
kimbyeongmo <kbmomo9999@naver.com>
2024-06-19 09:35:21 +0900

    # Context API 기초

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import MainHeader from './components/SideEffect/MainHeader';
 import Home from './components/SideEffect/Home';
 import Login from './components/SideEffect/Login';
 
+import AuthContext from './store/auth-context';
+
 
 const App = () => {
 
@@ -24,6 +26,7 @@ const App = () => {
   // side effect 처리를 위한 함수
   // useEffect는 기본적으로 컴포넌트 렌더링시 단 한번만 호출
   useEffect(() => {
+    
     // console.log('로그인 검사 수행!');
     const storedLoginFlag = localStorage.getItem('login-flag');
     if (storedLoginFlag === '1') {
@@ -40,19 +43,22 @@ const App = () => {
     setIsloggedIn(true);
   };
 
+  // 로그아웃 실행 함수
   const logoutHandler = () => {
     localStorage.removeItem('login-flag');
     setIsloggedIn(false);
   };
 
   return (
-    <>
+    <AuthContext.Provider value={{
+      isLoggedIn: isLoggedIn
+    }}>
       <MainHeader onLogout={logoutHandler} />
       <main>
         {isLoggedIn && <Home />}
         {!isLoggedIn && <Login onLogin={loginHandler} />}
       </main>
-    </>
+    </AuthContext.Provider>
   );
 };
 
