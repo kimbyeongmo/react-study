60e3ce500141f69716d60d5c21e8ef9d4409a125
kimbyeongmo <kbmomo9999@naver.com>
2024-06-20 11:19:58 +0900

    # useRef를 통한 timer공유 방지

--- src/components/Ref/TimeChallenge.js
+++ src/components/Ref/TimeChallenge.js
@@ -1,8 +1,9 @@
-import React, { useState } from 'react';
-
-let timer;
+import React, { useRef, useState } from 'react';
 
 const TimerChallenge = ({ title, targetTime }) => {
+
+  // timer를 ref변수로 관리
+  const timer = useRef();
 
   // 타이머가 시작되었는지를 확인하는 상태값
   const [timerStarted, setTimerStarted] = useState(false);
@@ -10,23 +11,17 @@ const TimerChallenge = ({ title, targetTime }) => {
   // 타겟시간이 종료되었는지 여부
   const [timerExpired, setTimerExpired] = useState(false);
 
-  
-
   const startHandler = e => {
 
-    timer = setTimeout(() => {
+    timer.current = setTimeout(() => {
       setTimerExpired(true);
     }, targetTime * 1000);
 
     setTimerStarted(true);
   };
 
-  // 전역변수로 timer 설정시 5초 -> 1초 -> 1초 -> 5초를 연속클릭해보면
-  // 5초짜리 timer가 정상작동하지 않는 이유는
-  // 4개의 TimeChallenge컴포넌트가 1개의 timer를 공유하여
-  // 처음 5초짜리 timer가 1초짜리에 의해 덮어씌워지기 때문
   const stopHandler = e => {
-    clearTimeout(timer);
+    clearTimeout(timer.current);
   };
 
   return (
