ac2a8433bbdb6a55a53f065645f0e9398da4a531
kimbyeongmo <kbmomo9999@naver.com>
2024-06-20 10:30:31 +0900

    # Ref와 상태값 연습 예제 템플릿 생성

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
-import './App.css';
-
-// bootstrap
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-import TodoTemplate from './components/Todo/TodoTemplate';
+import React from 'react';
+import Player from './components/Ref/Player';
 
 const App = () => {
 
-  const onAdd=()=>{
-    
-  }
-
-  return <TodoTemplate onAdd={onAdd}/>;
+  return (
+    <>
+      <Player />
+      <div id="challenges"></div>
+    </>
+  );
 };
 
 export default App;
