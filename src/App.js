aea9457d50077fff4cc2365554d14e3ede9cebfe
kimbyeongmo <kbmomo9999@naver.com>
2024-07-02 11:47:17 +0900

    # 커스텀 에러페이지 # 상대경로로 링크 제어하기

--- src/App.js
+++ src/App.js
@@ -2,17 +2,19 @@ import React from 'react';
 import Home from './components/RouteExample/pages/Home';
 import Products from './components/RouteExample/pages/Products';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import RootLayout from './components/RouteExample/pages/layout/RootLayout';
+import RootLayout from './components/RouteExample/layout/RootLayout';
+import ErrorPage from './components/RouteExample/pages/ErrorPage';
 
 // 라우터 설정
 const router = createBrowserRouter([
 
   {
-    path: '/',
+    path: '/base',
     element: <RootLayout />,
+    errorElement : <ErrorPage />,
     children: [
-      { path: '/', element: <Home /> },
-      { path: '/products', element: <Products /> }
+      { path: '', element: <Home /> },
+      { path: 'products', element: <Products /> }
     ]
   }
  
