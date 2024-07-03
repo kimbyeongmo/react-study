26e1a5c9342f414a83b9bc3bcffc108b2ddcddb7
kimbyeongmo <kbmomo9999@naver.com>
2024-07-03 12:03:59 +0900

    # 에러페이지 커스텀 메시지 처리

--- src/components/RouteExample/pages/Events.js
+++ src/components/RouteExample/pages/Events.js
@@ -1,11 +1,12 @@
-import React from "react";
-import EventList from "../components/EventList";
-import { useLoaderData } from "react-router-dom";
+import React from 'react';
+import EventList from '../components/EventList';
+import { useLoaderData } from 'react-router-dom';
 
 const Events = () => {
+
   // loader가 리턴한 데이터 받아오기
   const eventList = useLoaderData();
-  // console.log(eventList);
+  console.log(eventList);
 
   return (
     <>
@@ -20,16 +21,16 @@ export default Events;
 // loader를 app.js로부터 아웃소싱
 export const loader = async () => {
 
-  // 이 페이지가 열릴때 자동으로 트리거되어 호출되는 함수
-  // 이 함수에는 페이지가 열리자마자 해야할 일을 적을 수 있습니다.
-
-  // console.log('loader call!!');
-  const response = await fetch("http://localhost:8282/events");
-  // const jsonData = await response.json();
-
-  // loader가 리턴한 데이터는 loader를 선언한 컴포넌트와 그 하위 컴포넌트에서
-  // 언제든 불러서 사용할 수 있다.
-
-  // loader에서 fetch의 결과를 바로 리턴하면 알아서 json을 추출해줌.
-  return response;
+  const response = await fetch('http://localhost:8282/events?sort=date');
+  
+  if (!response.ok) {
+    const errorText = await response.text();
+    throw new Response(
+      JSON.stringify({ message: errorText}),
+      {
+        status:response.status
+      }
+    );
+  } 
+  return response; // ok일 경우 events[]
 };
