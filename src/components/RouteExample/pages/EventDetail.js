fd5b8495ab4724403c2c18311e7fb5270ecf6969
kimbyeongmo <kbmomo9999@naver.com>
2024-07-04 10:23:33 +0900

    # loader로 상세조회하기

--- src/components/RouteExample/pages/EventDetail.js
+++ src/components/RouteExample/pages/EventDetail.js
@@ -1,24 +1,33 @@
-import React, { useEffect, useState } from 'react';
-import { useLoaderData, useParams } from 'react-router-dom';
+import React from 'react';
+import { useLoaderData } from 'react-router-dom';
 import EventItem from '../components/EventItem';
 
 const EventDetail = () => {
 
-  const { eventId: id } = useParams();
-  const [ev, setEv] = useState({});
-
-  useEffect(() => {
-
-    (async () => {
-      const response = await fetch(`http://localhost:8282/events/${id}`);
-      const json = await response.json();
-      console.log('json: ', json);
-      setEv(json);
-    })();
-
-  }, []);
+  const ev = useLoaderData();
 
   return <EventItem event={ev} />;
 };
 
 export default EventDetail;
+
+export const loader = async ({ params }) => {
+
+  const { eventId: id } = params;
+
+  // console.log('abc: ', abc.params.eventId);
+
+  // use로 시작하는 함수인 리액트 훅은 컴포넌트 내부에서만 사용가능
+  // const { eventId: id } = useParams();
+  // const [ev, setEv] = useState({});
+
+  const response = await fetch(`http://localhost:8282/events/${id}`);
+
+  if (!response.ok) {
+    // ... 예외처리
+  }
+
+  return await response.json();
+  // console.log('json: ', json);
+
+};
